489558d7b103f102fc22b91454cf959ea9d8e4d9
TeddyFitzPatrick <tfitz200@gmail.com>
2025-03-18 00:12:57 -0400

    added basic movement for white pieces

--- src/js/chess.js
+++ src/js/chess.js
@@ -1,24 +1,24 @@
-// Piece images
-const whitePawnImage = new Image(); whitePawnImage.src = "./assets/whitePawn.png";
-const blackPawnImage = new Image(); blackPawnImage.src = "./assets/blackPawn.png";
+/* Image assets for pieces */
+// Pawns
+const whitePawnImage = new Image(); whitePawnImage.src = "./src/assets/whitePawn.png";
+const blackPawnImage = new Image(); blackPawnImage.src = "./src/assets/blackPawn.png";
+// Bishops
+const whiteBishopImage = new Image(); whiteBishopImage.src = "./src/assets/whiteBishop.png";
+const blackBishopImage = new Image(); blackBishopImage.src = "./src/assets/blackBishop.png";
+// Knights
+const whiteKnightImage = new Image(); whiteKnightImage.src = "./src/assets/whiteKnight.png";
+const blackKnightImage = new Image(); blackKnightImage.src = "./src/assets/blackKnight.png";
+// Rooks
+const whiteRookImage = new Image(); whiteRookImage.src = "./src/assets/whiteRook.png";
+const blackRookImage = new Image(); blackRookImage.src = "./src/assets/blackRook.png";
+// Kings
+const whiteKingImage = new Image(); whiteKingImage.src = "./src/assets/whiteKing.png";
+const blackKingImage = new Image(); blackKingImage.src = "./src/assets/blackKing.png";
+// Queens
+const whiteQueenImage = new Image(); whiteQueenImage.src = "./src/assets/whiteQueen.png";
+const blackQueenImage = new Image(); blackQueenImage.src = "./src/assets/blackQueen.png";
 
-const whiteBishopImage = new Image(); whiteBishopImage.src = "./assets/whiteBishop.png";
-const blackBishopImage = new Image(); blackBishopImage.src = "./assets/blackBishop.png";
-
-const whiteKnightImage = new Image(); whiteKnightImage.src = "./assets/whiteKnight.png";
-const blackKnightImage = new Image(); blackKnightImage.src = "./assets/blackKnight.png";
-
-const whiteRookImage = new Image(); whiteRookImage.src = "./assets/whiteRook.png";
-const blackRookImage = new Image(); blackRookImage.src = "./assets/blackRook.png";
-
-const whiteKingImage = new Image(); whiteKingImage.src = "./assets/whiteKing.png";
-const blackKingImage = new Image(); blackKingImage.src = "./assets/blackKing.png";
-
-const whiteQueenImage = new Image(); whiteQueenImage.src = "./assets/whiteQueen.png";
-const blackQueenImage = new Image(); blackQueenImage.src = "./assets/blackQueen.png";
-
-
-// Piece encodings
+/* Piece encodings */
 const Piece = Object.freeze({
     EMPTY: 0,
     WHITE_PAWN: 1,
@@ -34,13 +34,47 @@ const Piece = Object.freeze({
     WHITE_KING: 6,
     BLACK_KING: -6
 });
+
+/* Piece Move Rules */
+const bishopMoveDirections = [
+    [-1, -1], [-1, 1], 
+    [1, -1], [1, 1]   
+];
+const knightMoveDirections = [
+    [-2, -1], [-2, 1], 
+    [-1, -2], [-1, 2],
+    [1, -2], [1, 2],   
+    [2, -1], [2, 1]    
+];
+const rookMoveDirections = [
+    [-1, 0], [1, 0], 
+    [0, -1], [0, 1]  
+];
+const queenMoveDirections = [
+    [-1, -1], [-1, 1], [1, -1], [1, 1], 
+    [-1, 0], [1, 0], [0, -1], [0, 1]    
+];
+const kingMoveDirections = [
+    [-1, -1], [-1, 1], [1, -1], [1, 1], 
+    [-1, 0], [1, 0], [0, -1], [0, 1]   
+];
+
 // Board State
 let board
 let canvas, ctx
 let boardLength
 
-let pieceHeld = null;
-let holdingPiece = false;
+// Player
+let playerColor = "white";
+
+// Gameplay
+let turnToMove = "white";
+let pieceHeldRank = null;
+let pieceHeldFile = null;
+let isHoldingPiece = false;
+
+// castling and en passant
+let kingMoved = false;
 
 window.onload = function () {
     // Initial board configuration
@@ -61,23 +95,149 @@ window.onload = function () {
         // Get the piece clicked
         const pieceClicked = board[rank][file];
         renderBoard();
-        
-        const legalMoves = getLegalMoves(rank, file);
-        for (const legalMove of legalMoves){
-            console.log(legalMove);
+
+        // If holding a piece
+        if (isHoldingPiece){
+            // Legal move is played
+            if (getLegalMoves(pieceHeldRank, pieceHeldFile).some(move => move[0] == rank && move[1] == file)){
+                isHoldingPiece = false;
+                // Move the piece from the old square to the new 
+                board[rank][file] = board[pieceHeldRank][pieceHeldFile];
+                board[pieceHeldRank][pieceHeldFile] = Piece.EMPTY;
+            }
+            // Reselect same piece to deselect
+            else if (rank == pieceHeldRank && file == pieceHeldFile){
+                isHoldingPiece = false;
+            }
+            // Select another piece
+            else if (pieceClicked > 0){
+                pickupPiece(rank, file);
+            }
+            // Empty spot clicked, deselect piece
+            else{
+                isHoldingPiece = false;
+            }
         }
-        
+        // Pick up a piece
+        else if (pieceClicked != Piece.EMPTY && pieceClicked > 0){
+            pickupPiece(rank, file);
+        }
+        console.log("HOLDING_PIECE: " + isHoldingPiece);
         renderPieces();
     });
 
     // rendering
-    renderBoard();
-    renderPieces();
+    render();
 }
 
+function pickupPiece(rank, file){
+    isHoldingPiece = true;
+    pieceHeldRank = rank;
+    pieceHeldFile = file;
+    // Highlight legal moves
+    ctx.strokeStyle = "lightgreen";
+    ctx.lineWidth = 8;
+    ctx.fillStyle = "lightgreen";
+    for (const [legalRank, legalFile] of getLegalMoves(rank, file)){
+        // Draw a circle around legal spots to move
+        // ctx.beginPath();
+        // ctx.arc(legalFile * 100+50, legalRank * 100+50, 45, 0, Math.PI * 2);
+        // ctx.stroke();
+        ctx.strokeRect(legalFile * 100 + 2.5, legalRank * 100 + 2.5, 95, 95);
+        // console.log(legalRank + ", " + legalFile); DEBUG
+    }
+}
 
 function getLegalMoves(rank, file){
-    return [(0, 0), (1, 1)];
+    // Get the piece type (Pawn, Bishop, Knight, ...)
+    const piece = board[rank][file];
+    let legalMoves = [];
+    // Ignore empty
+    if (piece == Piece.EMPTY) return legalMoves;
+    // Pawns
+    else if (piece == Piece.WHITE_PAWN){
+        // Move forward 1
+        if (isInBounds(rank-1, file) && board[rank-1][file] == Piece.EMPTY){
+            legalMoves.push([rank-1,file]);
+        }
+        // Move forward 2 
+        if (rank == 6 && isInBounds(rank-2, file) && board[rank-1][file] == Piece.EMPTY && board[rank-2][file] == Piece.EMPTY){
+            legalMoves.push([rank-2,file]);
+        }
+        // Captures
+        for (const df of [-1, 1]) {
+            if (isInBounds(rank - 1, file + df) && board[rank - 1][file + df] < 0) {
+                legalMoves.push([rank - 1, file + df]);
+            }
+        }
+        // TODO: En passant
+    }   
+    // Bishops
+    else if (piece == Piece.WHITE_BISHOP){
+        for (const [dr, df] of bishopMoveDirections) {
+            let newRank = rank + dr;
+            let newFile = file + df;
+            while (isInBounds(newRank, newFile) && board[newRank][newFile] <= 0) {
+                // Move to empty square
+                legalMoves.push([newRank, newFile]);
+                // Capture
+                if (board[newRank][newFile] < 0) break; 
+                newRank += dr;
+                newFile += df;
+            }
+        }
+    }
+    // Knights
+    else if (piece == Piece.WHITE_KNIGHT){
+        for (const [dr, df] of knightMoveDirections) {
+            const newRank = rank + dr;
+            const newFile = file + df;
+            if (isInBounds(newRank, newFile) && board[newRank][newFile] <= 0) {
+                legalMoves.push([newRank, newFile]);
+            }
+        }
+    }
+    // Rooks
+    else if (piece == Piece.WHITE_ROOK){
+        for (const [dr, df] of rookMoveDirections) {
+            let newRank = rank + dr;
+            let newFile = file + df;
+            while (isInBounds(newRank, newFile) && board[newRank][newFile] <= 0) {
+                // Move to empty square
+                legalMoves.push([newRank, newFile]);
+                // Capture
+                if (board[newRank][newFile] < 0) break;
+                newRank += dr;
+                newFile += df;
+            }
+        }
+    }
+    // Queens
+    else if (piece == Piece.WHITE_QUEEN) {
+        for (const [dr, df] of queenMoveDirections) {
+            let newRank = rank + dr;
+            let newFile = file + df;
+            while (isInBounds(newRank, newFile) && board[newRank][newFile] <= 0) {
+                // Move to empty
+                legalMoves.push([newRank, newFile]);
+                // Capture
+                if (board[newRank][newFile] < 0) break; 
+                newRank += dr;
+                newFile += df;
+            }
+        }
+    }
+    // Kings
+    else if (piece == Piece.WHITE_KING) {
+        for (const [dr, df] of kingMoveDirections) {
+            let newRank = rank + dr;
+            let newFile = file + df;
+            if (isInBounds(newRank, newFile) && board[newRank][newFile] <= 0) {
+                legalMoves.push([newRank, newFile]);
+            }
+        }
+    }
+    return legalMoves;
 }
 
 function resizeCanvas(){
@@ -90,6 +250,8 @@ function resizeCanvas(){
 
 function render(){
     //...
+    renderBoard();
+    renderPieces();
 }
 
 function renderBoard(){
@@ -106,46 +268,45 @@ function renderBoard(){
     }
 }
 
-function renderPieces(){
+function renderPieces() {
     // Render the pieces
-    for (let rank = 0; rank <= 7; rank++){
-        for (let file = 0; file <= 7; file++){
-            // Draw the piece on the square (if there is one)
+    for (let rank = 0; rank <= 7; rank++) {
+        for (let file = 0; file <= 7; file++) {
             switch (board[rank][file]) {
-                case 1:
+                case Piece.WHITE_PAWN:
                     ctx.drawImage(whitePawnImage, file * 100, rank * 100, 100, 100);
                     break;
-                case -1:
+                case Piece.BLACK_PAWN:
                     ctx.drawImage(blackPawnImage, file * 100, rank * 100, 100, 100);
                     break;
-                case 2:
+                case Piece.WHITE_KNIGHT:
                     ctx.drawImage(whiteKnightImage, file * 100, rank * 100, 100, 100);
                     break;
-                case -2:
+                case Piece.BLACK_KNIGHT:
                     ctx.drawImage(blackKnightImage, file * 100, rank * 100, 100, 100);
                     break;
-                case 3:
+                case Piece.WHITE_BISHOP:
                     ctx.drawImage(whiteBishopImage, file * 100, rank * 100, 100, 100);
                     break;
-                case -3:
+                case Piece.BLACK_BISHOP:
                     ctx.drawImage(blackBishopImage, file * 100, rank * 100, 100, 100);
                     break;
-                case 4:
+                case Piece.WHITE_ROOK:
                     ctx.drawImage(whiteRookImage, file * 100, rank * 100, 100, 100);
                     break;
-                case -4:
+                case Piece.BLACK_ROOK:
                     ctx.drawImage(blackRookImage, file * 100, rank * 100, 100, 100);
                     break;
-                case 5:
+                case Piece.WHITE_QUEEN:
                     ctx.drawImage(whiteQueenImage, file * 100, rank * 100, 100, 100);
                     break;
-                case -5:
+                case Piece.BLACK_QUEEN:
                     ctx.drawImage(blackQueenImage, file * 100, rank * 100, 100, 100);
                     break;
-                case 6:
+                case Piece.WHITE_KING:
                     ctx.drawImage(whiteKingImage, file * 100, rank * 100, 100, 100);
                     break;
-                case -6:
+                case Piece.BLACK_KING:
                     ctx.drawImage(blackKingImage, file * 100, rank * 100, 100, 100);
                     break;
             }
@@ -159,9 +320,13 @@ function resetBoard(){
         [-1, -1, -1, -1, -1, -1, -1, -1],
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, -1, 0, 0, 0],
+        [0, 0, 0, 0, 0, -1, 0, 0],
+        [1, 1, 1, 1, 0, 1, 1, 1],
         [4, 2, 3, 5, 6, 3, 2, 4]
     ]
+}
+
+function isInBounds(rank, file){
+    return (rank >= 0 && rank <= 7) && (file >= 0 && file <= 7);
 }
