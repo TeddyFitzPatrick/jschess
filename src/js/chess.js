6555308f86f60ed5b8b7b49deee6e8277c1a7ec4
TeddyFitzPatrick <tfitz200@gmail.com>
2025-03-19 20:58:50 -0400

    added castling conditions and firebase POST functions

--- src/js/chess.js
+++ src/js/chess.js
@@ -18,7 +18,7 @@ const blackKingImage = new Image(); blackKingImage.src = "./src/assets/blackKing
 const whiteQueenImage = new Image(); whiteQueenImage.src = "./src/assets/whiteQueen.png";
 const blackQueenImage = new Image(); blackQueenImage.src = "./src/assets/blackQueen.png";
 
-/* Piece encodings */
+/* Encodings */
 const Piece = Object.freeze({
     EMPTY: 0,
     WHITE_PAWN: 1,
@@ -34,6 +34,10 @@ const Piece = Object.freeze({
     WHITE_KING: 6,
     BLACK_KING: -6
 });
+const Color = Object.freeze({
+    WHITE: 1,
+    BLACK: -1
+})
 
 /* Piece Move Rules */
 const bishopMoveDirections = [
@@ -60,7 +64,7 @@ const kingMoveDirections = [
 ];
 
 // Board State
-let board
+export let board
 let canvas, ctx
 let boardLength
 /* Castling */
@@ -70,25 +74,38 @@ let whiteLongRookMoved = false;
 let blackKingMoved = false;
 let blackShortRookMoved = false;
 let blackLongRookMoved = false;
-
 // Game play
-let playerColor = 1; // -1 (black), 1 (white)
-let turnToMove = 1;  // -1 (black), 1 (white)
+let playerColor = Color.WHITE; // -1 (black), 1 (white)
+let turnToMove;  // -1 (black), 1 (white)
 let pieceHeldRank = null;
 let pieceHeldFile = null;
 let isHoldingPiece = false;
+let gameOver = false;
+// Move order
+// movesPlayed = [];
 
 // Canvas Rendering
 const TILE_SIZE = 100;
+// Game results
+let gameOverMessage;
+const notification = document.getElementById("notification");
+const winner = document.getElementById("winner");
+const subtext = document.getElementById("subtext");
 
-window.onload = function () {
-    // Initial board layout
-    resetBoard();
-    // Set up canvas rendering
-    resizeCanvas();
-    // Event listeners
+// Firebase
+const DB_URL = "https://struglauk-default-rtdb.firebaseio.com/.json";
+
+window.onload = async function () {
+    // Add event listeners
+    const restartButton = document.getElementById("restartButton");
+    restartButton.addEventListener("click", function(){
+        gameOver = false;
+        hideResults();
+        gameLoop();
+    });
     window.addEventListener("resize", resizeCanvas());
     window.addEventListener("click", (event) => {
+        if (gameOver) return;
         // Get the rank and file of the mouse click
         const rect = canvas.getBoundingClientRect();
         const mouseX = event.clientX - rect.left;
@@ -102,6 +119,13 @@ window.onload = function () {
             // Play a legal move
             if (getLegalMoves(pieceHeldRank, pieceHeldFile).some(move => move[0] == rank && move[1] == file)){
                 movePiece(pieceHeldRank, pieceHeldFile, rank, file);
+                // Check if the move played checkmated the opponents
+                if (isCheckmate()){
+                    gameOver = true;
+                    gameOverMessage = `${turnToMove == Color.WHITE ? "Black" : "White"} wins by checkmate`;
+                    showResults();
+                }
+                // TODO: Store the move played
             }
             // Reselect same piece to deselect
             else if (rank == pieceHeldRank && file == pieceHeldFile){
@@ -119,13 +143,102 @@ window.onload = function () {
         // Pick up a piece
         else if (pieceClicked != Piece.EMPTY && Math.sign(pieceClicked) === Math.sign(turnToMove)){
             pickupPiece(rank, file);
-        }
-        // DEBUG
-        console.log("HOLDING_PIECE: " + isHoldingPiece);
-        console.log("Piece: " + board[pieceHeldRank][pieceHeldFile] + " at (" + pieceHeldRank + ", " + pieceHeldFile + ")");
-        console.log("TurnToMove: " + turnToMove);
+        }       
+        // Render the pieces
         renderPieces();
     });
+
+    
+
+    // Start game
+    gameLoop();
+}
+
+
+async function clearDatabase(){
+    fetch(DB_URL, {
+        method: 'DELETE', 
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+        console.error('Failed to delete move, status:', response.status);
+        }
+    })
+    .catch(error => {
+        console.error('Error deleting move:', error);
+    });
+}
+
+async function readMove(){
+    const move = fetch(DB_URL)
+        .then(response => response.json())
+        .then(data => {
+            // Number of messages
+            let size = 0;
+            for (let key in data){
+                size+=1;
+            }
+            if (size == 0) console.log("no moves");
+            else if (size > 1) console.log("too many moves");
+            for (let key in data){
+                console.log("FROM: " + data[key]["from"]);
+                console.log("TO: " + data[key]["to"]);
+            }
+        })
+        .catch(error => {
+            console.error('Error reading moves:', error);
+        });
+}
+
+async function sendMove(moveData){
+    fetch(DB_URL, {
+    method: 'POST', 
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(moveData)  
+    })
+    .then(response => response.json())  
+    .then(data => {
+        console.log('Move added successfully:', data);
+    })
+    .catch(error => {
+        console.error('Error adding move:', error);
+    });
+}
+
+async function gameLoop(){
+    // Initial board layout
+    resetBoard();
+    // White moves first
+    turnToMove = Color.WHITE;
+    // Set up canvas rendering
+    resizeCanvas();
+
+    // TODO: Show previous game states
+    window.addEventListener("keydown", function (event){
+        if (event.key === "ArrowLeft"){
+            console.log("YIP{EEE");
+        } else if (event.key === "~"){
+            clearDatabase();
+        }
+    });
+
+    const exampleMove = {
+        "from": [3, 4],
+        "to": [3, 6]
+    }
+    // readMove(exampleMove);
+    // sendMove(exampleMove);
+    
+    // Control loop
+    while (!gameOver){
+        
+    }
+
     render();
 }
 
@@ -142,8 +255,17 @@ function movePiece(previousRank, previousFile, newRank, newFile){
         board[7][3] = Piece.WHITE_ROOK;
         board[7][0] = Piece.EMPTY;
     }
+    // Black short castle
+    else if (pieceMoved == Piece.BLACK_KING && previousFile + 2 == newFile){
+        board[0][5] = Piece.BLACK_ROOK; 
+        board[0][7] = Piece.EMPTY;
+    }
+    // Black long castle
+    else if (pieceMoved == Piece.BLACK_KING && previousFile - 2 == newFile){
+        board[0][3] = Piece.BLACK_ROOK;
+        board[0][0] = Piece.EMPTY;
+    }
     // TODO: black castle
-
 
     /* Keep track if the king or rook moved to disqualifying castling */
     // King moved
@@ -183,12 +305,27 @@ function pickupPiece(rank, file){
     }
 }
 
+function isCheckmate(){
+    for (let rank = 0; rank <= 7; rank++){
+        for (let file = 0; file <= 7; file++){
+            if (Math.sign(board[rank][file]) == turnToMove){
+                // If one of the player's pieces has a legal move, checkmate has not occurred 
+                if (getLegalMoves(rank, file).length > 0){
+                    return false
+                }
+            }
+        }
+    }
+    return true;
+}
+
 function getLegalMoves(rank, file){
     // Get the set of all moves possible for a piece at a given rank and file
     let allMoves = getAllMoves(rank, file);
     let legalMoves = [];
     const piece = board[rank][file];
-    // Moves are legal if they do not allow the opponent a move that captures the king 
+
+    /* Remove moves that put the king in check */
     for (const move of allMoves){
         let legalMove = true;
         let newRank = move[0], newFile = move[1];
@@ -198,34 +335,16 @@ function getLegalMoves(rank, file){
         board[rank][file] = Piece.EMPTY; 
         /* Check all legal moves available to the opponent to see if any capture the king */
         // Find the king's positions
-        let kingRank, kingFile
-        for (let searchRank = 0; searchRank <= 7; searchRank++) {newRank
-            for (let searchFile = 0; searchFile <= 7; searchFile++) {
-                let searchPiece = board[searchRank][searchFile];
-                if (Math.abs(searchPiece) == Math.abs(Piece.WHITE_KING) && Math.sign(searchPiece) == Math.sign(piece)){
-                    kingRank = searchRank; 
-                    kingFile = searchFile;
-                }
-            }
-        }
-        // Check if the opponent's legal moves include the king's coordinates (king is captured)
-        for (let searchRank = 0; searchRank <= 7; searchRank++) {
-            for (let searchFile = 0; searchFile <= 7; searchFile++) {
-                // The piece is the opponents if it has the opposite color
-                if (Math.sign(board[searchRank][searchFile]) == -Math.sign(piece)){
-                    for (let [opponentRank, opponentFile] of getAllMoves(searchRank, searchFile)){
-                        // Determine if the opponent's piece has a legal move that captures the king
-                        if (opponentRank == kingRank && opponentFile == kingFile) legalMove = false;
-                    }
-                }
-            }
-        }
+        let kingPos = findPiece(Math.sign(piece) * Math.abs(Piece.WHITE_KING));
+        // Check if the opponent's can capture the king after playing the move
+        if (isAttacked(kingPos[0], kingPos[1], -Math.sign(piece))) legalMove = false;
         // Undo the move
         board[rank][file] = board[newRank][newFile];
         board[newRank][newFile] = originalPiece; 
         // Add the move to the legal move set if does not expose the king
         if (legalMove) legalMoves.push(move);
     }
+
     return legalMoves;
 }
 
@@ -264,7 +383,7 @@ function getAllMoves(rank, file){
                 // Move to empty square
                 allMoves.push([newRank, newFile]);
                 // Capture
-                if (board[newRank][newFile] < 0) break; 
+                if (board[newRank][newFile] != 0) break; 
                 newRank += dr;
                 newFile += df;
             }
@@ -320,16 +439,64 @@ function getAllMoves(rank, file){
                 allMoves.push([newRank, newFile]);
             }
         }
-        // Short castle
-        if (!whiteKingMoved && !whiteShortRookMoved && board[7][5] == Piece.EMPTY && board[7][6] == Piece.EMPTY){
+        // White Short Castle
+        if (!whiteKingMoved && !whiteShortRookMoved &&
+            !isAttacked(7, 5, -1, true) && 
+            board[7][5] == Piece.EMPTY && board[7][6] == Piece.EMPTY){
             allMoves.push([7, 6]);
         }
-        // Long Castle
-        if (!whiteKingMoved && !whiteLongRookMoved && board[7][1] == Piece.EMPTY && board[7][2] == Piece.EMPTY && board[7][3] == Piece.EMPTY){
+        // White Long Castle
+        if (!whiteKingMoved && !whiteLongRookMoved &&
+            !isAttacked(7, 3, -1, true) &&
+            board[7][1] == Piece.EMPTY && board[7][2] == Piece.EMPTY && board[7][3] == Piece.EMPTY){
             allMoves.push([7, 2]);
+        }
+        // Black Short Castle
+        if (!blackKingMoved && !blackShortRookMoved &&
+            !isAttacked(0, 5, 1, true) &&
+            board[0][5] == Piece.EMPTY && board[0][6] == Piece.EMPTY){
+            allMoves.push([0, 6]);
+        }
+        // Black Long Castle
+        if (!blackKingMoved && !blackLongRookMoved &&
+            !isAttacked(0, 3, 1, true) &&
+            board[0][1] == Piece.EMPTY && board[0][2] == Piece.EMPTY && board[0][3] == Piece.EMPTY){
+            allMoves.push([0, 2]);
         }
     }
     return allMoves;
+}
+
+function isAttacked(rank, file, opponentColor, castleCheck=false){
+    for (let searchRank = 0; searchRank <= 7; searchRank++){
+        for (let searchFile = 0; searchFile <= 7; searchFile++){
+            let searchPiece = board[searchRank][searchFile];
+            // Look for opponent piece's legal moves
+            if (Math.sign(searchPiece) == opponentColor){
+                // Ignore king moves to avoid infinite recursion in castle eligibility check
+                if (castleCheck && Math.abs(searchPiece) == Math.abs(Piece.WHITE_KING)) continue;
+                for (let [attackedRank, attackedFile] of getAllMoves(searchRank, searchFile)){
+                    // If any of the opponent's legal moves can reach the given square, it is attacked
+                    if (attackedRank == rank && attackedFile == file){
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function findPiece(piece){
+    let rank, file
+    for (let searchRank = 0; searchRank <= 7; searchRank++) {
+        for (let searchFile = 0; searchFile <= 7; searchFile++) {
+            if (board[searchRank][searchFile] == piece){
+                return [searchRank, searchFile];
+            }
+        }
+    }
+    return [];
 }
 
 function resetBoard(){
@@ -343,6 +510,16 @@ function resetBoard(){
         [1, 1, 1, 1, 1, 1, 1, 1],
         [4, 2, 3, 5, 6, 3, 2, 4]
     ]
+    /*   REFERENCE DEFAULT GAME BOARD
+        [-4, -2, -3, -5, -6, -3, -2, -4],
+        [-1, -1, -1, -1, -1, -1, -1, -1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [4, 2, 3, 5, 6, 3, 2, 4]
+    */
 }
 
 function isInBounds(rank, file){
@@ -412,6 +589,8 @@ function renderPieces() {
             }
             // Render the piece
             ctx.drawImage(pieceImg, file * TILE_SIZE, rank * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+
+            // ctx.drawImage(pieceImg, (7-file) * TILE_SIZE, (7-rank) * TILE_SIZE, TILE_SIZE, TILE_SIZE);
         }
     }
 }
@@ -429,3 +608,12 @@ function renderBoard(){
         }
     }
 }
+
+function showResults(){
+    notification.style.display = "flex";
+    winner.innerText = gameOverMessage;
+}
+
+function hideResults(){
+    notification.style.display = "none";
+}  
